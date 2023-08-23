babae5e1b1ad1f3b758c30e484733197e90dcc34
Nacho228 <6319@holbertonstudents.com>
2023-08-23 11:33:49 -0700

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python3
-"""  for a given employee ID, returns information
-about his/her TODO list progress. """
+"""
+Employee Info
+for a given employee ID, returns information
+about his/her TODO list progress."""
 
 import requests
 import sys
 
-u = sys.argv[1]
 
-r = requests.get(f'https://jsonplaceholder.typicode.com/users/{u}/todos')
-user_info = requests.get(f'https://jsonplaceholder.typicode.com/users/{u}')
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        exit()
 
-todo_list = r.json()
-user_info_list = user_info.json()
+    u = sys.argv[1]
 
-user_name = user_info_list["name"]
+    r = requests.get(f'https://jsonplaceholder.typicode.com/users/{u}/todos')
+    user_info = requests.get(f'https://jsonplaceholder.typicode.com/users/{u}')
 
-completed = [todo for todo in todo_list if todo["completed"]]
-total_number = len(todo_list)
-completed_n = len(completed)
+    todo_list = r.json()
+    user_info_list = user_info.json()
 
+    user_name = user_info_list["name"]
 
-print(f"Employee {user_name} is done with tasks {completed_n}/{total_number}")
+    completed = [todo for todo in todo_list if todo["completed"]]
+    total_n = len(todo_list)
+    completed_n = len(completed)
 
-for todo in todo_list:
-    print("    ", todo['title'])
+    print(f"Employee {user_name} is done with tasks {completed_n}/{total_n}")
+
+    for todo in todo_list:
+        print("    ", todo['title'])
