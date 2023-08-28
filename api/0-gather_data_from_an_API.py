0e406abae12616d5ec92fc8e762abd8a2b75a93c
Nacho228 <6319@holbertonstudents.com>
2023-08-27 19:12:12 -0700

    2

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,34 +1,35 @@
 #!/usr/bin/python3
 """
-Employee Info
-for a given employee ID, returns information
-about his/her TODO list progress."""
-
+Task #0. Gather data from an API.
+Script that using jsonplaceholder.typicode.com API,
+for a given employee ID,returns information about
+his/her TODO list progress.
+"""
 import json
 import requests
 import sys
-
+import urllib
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        exit()
+    if len(sys.argv) != 2:
+        sys.exit(1)
 
-    u = int(sys.argv[1])
-
+    u = sys.argv[1]
     r = requests.get(f'https://jsonplaceholder.typicode.com/users/{u}/todos')
     user_info = requests.get(f'https://jsonplaceholder.typicode.com/users/{u}')
 
     todo_list = r.json()
-    user_info_list = user_info.json()
+    user_data = user_info.json()
 
-    user_name = user_info_list["name"]
+    user_name = user_data["name"]
 
     tasks_done = [task for task in todo_list if task["completed"]]
-    tn = len(todo_list)
-    completed_n = len(tasks_done)
+    count_tasks = len(todo_list)
+    completed_tasks = len(tasks_done)
 
     print("Employee {} is done with tasks({}/{}):"
-          .format(user_name, completed_n, tn))
+          .format(user_name, completed_tasks, count_tasks))
 
     for todo in todo_list:
-        print('\t', todo['title'])
+        if todo["completed"]:
+            print("\t{}".format(todo["title"]))
--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -3,11 +3,9 @@
 Employee Info
 for a given employee ID, returns information
 about his/her TODO list progress."""
-
 import json
 import requests
 import sys
-
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -22,6 +20,19 @@ if __name__ == "__main__":
     user_data = user_info.json()
 
     user_name = user_data["name"]
+
+    tasks_done = [task for task in todo_list if task["completed"]]
+    tn = len(todo_list)
+    completed_n = len(tasks_done)
+
+    file_name = '{}.json'.format(user_data.get('id'))
+
+    with open(file_name, mode="a", encoding="utf-8") as f:
+        f.write(json.dumps({u: [{
+            "task": todo.get("title"),
+            "completed": todo.get("completed"),
+            "username": todo.get("username"),
+            } for todo in todo_list]}))
 
     tasks_done = [task for task in todo_list if task["completed"]]
     tn = len(todo_list)
